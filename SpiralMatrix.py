3c1d87deabbfc361b45f7fd593f1c138da398ed2
Harsh Dwivedi <harshdwdi@gmail.com>
2026-08-04 00:55:02 +0530

    Generate Parantheses

---
+++ SpiralMatrix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        result = []
+
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+
+        while top <= bottom and left <= right:
+            # Left to Right
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1
+
+            # Top to Bottom
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+
+            if top <= bottom:
+                # Right to Left
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                # Bottom to Top
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+
+        return result
