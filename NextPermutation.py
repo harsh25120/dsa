e3e79108f34c92c2a0834448a511703a7b691058
Harsh Dwivedi <harshdwdi@gmail.com>
2026-08-05 01:21:38 +0530

    Next permutation

---
+++ NextPermutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+
+        # Find first decreasing element from the right
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        if i >= 0:
+            j = len(nums) - 1
+
+            # Find the next larger element
+            while nums[j] <= nums[i]:
+                j -= 1
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # Reverse the suffix
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
